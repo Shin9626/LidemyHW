fb962f407682932b1ec0d81f06e0cc6d9979b5c5
SHIN <akiyachiumi@gmail.com>
2022-03-24 18:09:34 +0800

    comment board Live Alone constructed

---
+++ W9/live_alone/index.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>LiveAlone 邊緣世界</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <header>
+        Welcome to LiveAlone! 歡迎來到邊緣世界
+    </header>
+    <main class="board">
+        <form method="POST" action="" class="board__form">
+            <div class="board__title">
+                Comments
+                <div class="board__login">
+                    會員登入/註冊
+                </div>
+            </div>
+            <div class="board__nickname">
+                暱稱： <input type="text" name="nickname">
+            </div>
+            <div>
+                <textarea id="board__form-text" rows="10" name="content"></textarea>
+            </div>
+            <div class="board__submit-box">
+                <input type="button" value="送出" class="board__submit-btn">
+            </div>
+        </form>
+        <div class="board__hr"></div>
+        <section>
+            <div class="card">
+                <div class="card__img">
+
+                </div>
+                <div class="card__body">
+                    <div class="card__info">
+                        <span class="card__author">戈登</span>
+                        <span class="card__time">2022-03-24 18:18:18</span>
+                    </div>
+                    <div class="card__content">
+                        我是邊緣人我是邊緣人我是邊緣人我是邊緣人我是邊緣人我是邊緣人我是邊緣人
+                    </div>
+                </div>
+            </div>
+            <div class="card">
+                <div class="card__img">
+
+                </div>
+                <div class="card__body">
+                    <div class="card__info">
+                        <span class="card__author">戈登</span>
+                        <span class="card__time">2022-03-24 18:18:18</span>
+                    </div>
+                    <div class="card__content">
+                        我是邊緣人我是邊緣人我是邊緣人我是邊緣人我是邊緣人我是邊緣人我是邊緣人我是邊緣人我是邊緣人我是邊緣人我是邊緣人我是邊緣人我是邊緣人我是邊緣人
+                    </div>
+                </div>
+            </div>
+            <div class="card">
+                <div class="card__img">
+
+                </div>
+                <div class="card__body">
+                    <div class="card__info">
+                        <span class="card__author">戈登</span>
+                        <span class="card__time">2022-03-24 18:18:18</span>
+                    </div>
+                    <div class="card__content">
+                        我是邊緣人我是邊緣人我是邊緣人我是邊緣人我是邊緣人我是邊緣人我是邊緣人我是邊緣人我是邊緣人我是邊緣人我是邊緣人我是邊緣人我是邊緣人我是邊緣人我是邊緣人我是邊緣人我是邊緣人我是邊緣人我是邊緣人我是邊緣人我是邊緣人
+                    </div>
+                </div>
+            </div>
+        </section>
+    </main>
+</body>
+
+</html>

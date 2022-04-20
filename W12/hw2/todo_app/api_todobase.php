e85f17b2ef0d51a419a6349b604bf9966d541863
SHIN <akiyachiumi@gmail.com>
2022-04-20 17:33:14 +0800

    todo conpleted

--- W12/hw2/todo_app/api_todobase.php
+++ W12/hw2/todo_app/api_todobase.php
@@ -1,23 +1,35 @@
 <?php
     require_once('conn.php');
 
-    header('Content-Type: text/html; charset=UTF-8'); 
+    header('Content-Type: application/x-www-form-urlencoded; charset=UTF-8'); 
     header('Access-Control-Allow-Origin: *');
 
-    if($_SERVER['REQUEST_METHOD'] == 'POST'){
-        
-        $content = $_POST['data'];
-        
-        $stmt = $conn->prepare("INSERT INTO todos(content) VALUES(?)");
-        $stmt->bind_param('s', $content);
-        $result = $stmt->execute();
 
-        $stmt = $conn->prepare("SELECT id FROM todos WHERE content=?");
-        $stmt->bind_param('s', $content);
-        $result = $stmt->execute();
-        $result = $stmt->get_result();
-        $row = $result->fetch_assoc();
-        echo $row['id'];
+    if($_SERVER['REQUEST_METHOD'] == 'POST'){
+        if(empty($_GET['id'])){
+            $content = json_encode($_POST['data'], JSON_UNESCAPED_UNICODE);
+        
+            $stmt = $conn->prepare("INSERT INTO todos(content) VALUES(?)");
+            $stmt->bind_param('s', $content);
+            $result = $stmt->execute();
+
+            $stmt = $conn->prepare("SELECT id FROM todos WHERE content=?");
+            $stmt->bind_param('s', $content);
+            $result = $stmt->execute();
+            $result = $stmt->get_result();
+            $row = $result->fetch_assoc();
+            echo $row['id'];
+        } else {
+            $id = $_GET['id'];
+
+            $content = json_encode($_POST['data'], JSON_UNESCAPED_UNICODE);
+        
+            $stmt = $conn->prepare("UPDATE todos SET content=? WHERE id=?");
+            $stmt->bind_param('si', $content, $id);
+            $result = $stmt->execute();
+
+            echo $id;
+        }             
     }
     
     if($_SERVER['REQUEST_METHOD'] == 'GET' && !empty($_GET['id'])){
@@ -28,6 +40,6 @@
         $result = $stmt->execute();
         $result = $stmt->get_result();
         $row = $result->fetch_assoc();
-        echo $row['content'];
+        echo json_decode($row['content']);
     }
 ?>

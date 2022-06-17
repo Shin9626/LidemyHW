220a9474506427242fc3b8d4ff2894fb0f041604
SHIN <akiyachiumi@gmail.com>
2022-06-17 18:26:57 +0800

    express-session

--- W17/eg1/app.js
+++ W17/eg1/app.js
@@ -1,17 +1,40 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const session = require('express-session')
 const db = require('./db')
 const todoController = require('./controllers/todoController')
 const app = express()
 const port = 5001
 
 app.use(express.static(__dirname));
-app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: true,
+    //cookie: { secure: true }
+}))
+
+app.set('view engine', 'ejs');
 
 app.get('/home', todoController.get);
+app.get('/login', (req, res) => {
+    res.render('login')
+});
+app.get('/logout', (req, res) => {
+    req.session.isLogin = false;
+    res.redirect('/home');
+});
 app.post('/home', todoController.add);
+app.post('/login', (req, res) => {
+    if (req.body.password === 'abc') {
+        req.session.isLogin = true;
+        res.redirect('/home')
+    } else {
+        res.redirect('/login')
+    }
+});
 
 app.listen(port, () => {
     db.connect((err) => {
--- W17/eg1/controllers/todoController.js
+++ W17/eg1/controllers/todoController.js
@@ -1,9 +1,6 @@
 const todoModel = require('../models/todoModel')
 
 const todoController = {
-    getNew: (req, res) => {
-        res.render('index')
-    },
 
     add: (req, res) => {
         const content = req.body.data;
@@ -20,12 +17,13 @@ const todoController = {
             const id = req.query.id
             todoModel.get(id, (err, results) => {
                 if (err) return console.log(err);
-                res.render('index', { todo: results[0] })
+                res.render('index', { todo: results[0], isLogin: req.session.isLogin })
             })
         } else {
-            res.render('index', { todo: false, data_id: false })
+            console.log(req.session.isLogin)
+            res.render('index', { todo: false, data_id: false, isLogin: req.session.isLogin })
         }
-    }
+    },
 }
 
 module.exports = todoController;
